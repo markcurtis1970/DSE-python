f8fc22c42c701d5f968be41ac80f382e9f261415
markcurtis1970 <mark.curtis@datastax.com>
2015-12-10 10:13:15 +0000

    removed notes from bottom of script

--- copy_and_scramble.py
+++ copy_and_scramble.py
@@ -75,14 +75,3 @@ prep_statement = createPrepStatement(results)
 # Write out the results
 write_copy(results, prep_statement)
 
-
-# Notes:
-#
-# 1. I was getting the following error: cassandra.protocol.SyntaxException: <ErrorMessage code=2000 [Syntax error in CQL query] message="line 1:4101 no viable alternative at input 'token' (...subMerchantId,taxAmount,taxAmountX,taxRate,tdSis,[token],transactionChannel...)">
-#
-# this is because the work "token" is a keyword, the [ ] around the field are just the server highlighting it, so Adam H mentioned:
-# from cassandra.metadata import protect_name
-# protect_name(<field name>)
-#
-# 2. I was seeing the following error: ValueError: Too many arguments provided to bind() (got 1507, expected 227)
-# this is because the prepared statement doesnt like a string sequence you need either a list or tuple
